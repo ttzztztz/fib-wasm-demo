45b4b25da12b9e34e87a1eb2fdb1500e26cd19a0
ttzztztz <ttzztztz@outlook.com>
2020-09-10 19:27:52 +0800

    chore: req

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,44 @@
 extern crate wasm_bindgen;
 
 use wasm_bindgen::prelude::*;
+use serde::{Deserialize, Serialize};
+use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{Request, RequestInit, RequestMode, Response};
+
+#[derive(Deserialize, Serialize, Clone)]
+struct GithubFollowerInfo {
+    login: String,
+    id: u64,
+}
 
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = console, js_name = log)]
     fn log(s: &str);
+}
+
+#[wasm_bindgen]
+pub async fn get_github_info() -> Result<JsValue, JsValue> {
+    let mut opts = RequestInit::new();
+    opts.method("GET");
+    opts.mode(RequestMode::Cors);
+
+    let url = format!("https://api.github.com/users/{username}/following?per_page={per_page}&page={page}",
+        username = "ttzztztz",
+        per_page = 100,
+        page = 1);
+
+    let request = Request::new_with_str_and_init(&url, &opts)?;
+    request.headers().set("Accept", "application/vnd.github.v3+json");
+    request.headers().set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36");
+
+    let window = web_sys::window().unwrap();
+    let resp_val = JsFuture::from(window.fetch_with_request(&request)).await?;
+
+    let json = JsFuture::from(resp.json()?).await?;
+
+    Ok(JsValue::from_serde(&res).unwrap())
 }
 
 #[wasm_bindgen]
